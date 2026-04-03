9c9f3314fdbe42a84658e3b9fa2fbb99ea0498bd
mamitfyy1 <oymakmuhammetali1@gmail.com>
2026-04-03 19:20:50 +0300

    layout2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 
 import "../style/globals.css";
 import "../style/projects.css";
